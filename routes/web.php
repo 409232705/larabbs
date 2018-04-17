470103da538ae5df40392d3714e18facddf71bf3
wyq <409232705@qq.com>
2018-04-17 23:34:16 +0800

    Finish user auth

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,20 @@
 
 
 Route::get('/', 'PagesController@root')->name('root');
+
+//Auth::routes(); 等同于以下代码
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+//end等同于以下代码
