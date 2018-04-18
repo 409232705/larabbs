9f1f4cca87b12ac9b326d4b3114c635a79692805
wyq <409232705@qq.com>
2018-04-18 22:23:57 +0800

    User profile page

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+
+    public function edit($id)
+    {
+        //
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+}
