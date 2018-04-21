6ae651199030833fd559315373f032dbe1d83391
wyq <409232705@qq.com>
2018-04-21 21:51:08 +0800

    Only authorized user can edit user profile

--- storage/framework/views/9a94243ffec9992c73eac5df4c91d73192d08981.php
+++ storage/framework/views/9a94243ffec9992c73eac5df4c91d73192d08981.php
@@ -32,7 +32,7 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
-                                <img src="<?php echo e($user->avatar); ?>" class="img-responsive img-circle" width="30px" height="30px">
+                                <img src="<?php if(isset($user)): ?> $user->avatar <?php endif; ?>" class="img-responsive img-circle" width="30px" height="30px">
                             </span>
                             <?php echo e(Auth::user()->name); ?> <span class="caret"></span>
                         </a>
