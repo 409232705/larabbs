99eadaf8e4cac9ea2375cb0e8e9653271b053bde
wyq <409232705@qq.com>
2018-04-23 21:59:33 +0800

    Users can edit their own topic

--- storage/framework/views/0c475bac865e9613fa21cf8a542045145287defb.php
+++ storage/framework/views/0c475bac865e9613fa21cf8a542045145287defb.php
@@ -33,13 +33,13 @@
 
                                     <div class="form-group">
                                         <select class="form-control" name="category_id" required>
-                                            <option value="" hidden disabled selected>请选择分类</option>
+                                            <option value="" hidden disabled <?php echo e($topic->id ? '' : 'selected'); ?>>请选择分类</option>
                                             <?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $value): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                                                <option value="<?php echo e($value->id); ?>"><?php echo e($value->name); ?></option>
+                                                <option value="<?php echo e($value->id); ?>" <?php echo e($topic->category_id == $value->id ? 'selected' : ''); ?>><?php echo e($value->name); ?></option>
                                             <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                                         </select>
                                     </div>
-
+                                    
                                     <div class="form-group">
                                         <textarea name="body" class="form-control" id="editor" rows="3" placeholder="请填入至少三个字符的内容。" required><?php echo e(old('body', $topic->body )); ?></textarea>
                                     </div>
